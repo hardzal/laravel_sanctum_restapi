ccf938c775a0c09dca4f1bc197fa8a74ed272a75
hardzal <suryadijogja@gmail.com>
2022-01-21 08:27:28 +0700

    add register api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('auth/register', \App\Http\Controllers\Api\Auth\RegisterController::class);
+Route::post('auth/login', \App\Http\Controllers\Api\Auth\LoginController::class);
